0c262963fa88f7bde3fb46bd92f4079a6196a556
Tom Tang <tomtang0406@gmail.com>
2022-05-23 01:44:57 +0800

    Synchronizing flow

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTaskTester {
+    public class Program {
+        static void Main() {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 1");
+
+            DoSomething();
+
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 4");
+            Console.ReadKey();
+        }
+
+        private static void DoSomething() {
+            Thread.Sleep(1000);
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 2");
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 3");
+        }
+    }
+}
