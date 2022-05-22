abda55e07cc00151db56fab0a6bff7c57af63933
Tom Tang <tomtang0406@gmail.com>
2022-05-23 02:31:50 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,21 @@ using System.Threading.Tasks;
 
 namespace AsyncTaskTester {
     public class Program {
+        static DateTime begin;
         static void Main() {
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 1");
+            begin = DateTime.Now;
+            Console.WriteLine($"[{(DateTime.Now - begin).TotalMilliseconds:0000}][{Thread.CurrentThread.ManagedThreadId}] STEP - 1");
 
             DoSomething();
 
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 4");
+            Console.WriteLine($"[{(DateTime.Now - begin).TotalMilliseconds:0000}][{Thread.CurrentThread.ManagedThreadId}] STEP - 4");
             Console.ReadKey();
         }
 
         private static void DoSomething() {
             Thread.Sleep(1000);
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 2");
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}][{Thread.CurrentThread.ManagedThreadId}] STEP - 3");
+            Console.WriteLine($"[{(DateTime.Now - begin).TotalMilliseconds:0000}][{Thread.CurrentThread.ManagedThreadId}] STEP - 2");
+            Console.WriteLine($"[{(DateTime.Now - begin).TotalMilliseconds:0000}][{Thread.CurrentThread.ManagedThreadId}] STEP - 3");
         }
     }
 }
